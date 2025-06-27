c1018473f5989a1f8cb600c5cc020754fe7c893f
laho0011 <quentin.lahousse@etudiant.univ-reims.fr>
2025-06-27 16:43:07 +0200

    fichiers config et style

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [new URL('https://photoportfolioapi-production.up.railway.app/uploads/**')],
+    },
+};
 
 export default nextConfig;
