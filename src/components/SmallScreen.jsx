6e369183b2b2467b141f0dff940d8fdb87d2c202
laho0011 <quentin.lahousse@etudiant.univ-reims.fr>
2025-06-27 16:39:30 +0200

    création du composant d'affichage sur petit écran

---
+++ src/components/SmallScreen.jsx
@@ -0,0 +1,12 @@
+
+
+const SmallScreen = () => {
+    return (
+        <div style={{maxWidth:"80%", margin:"auto", textAlign:"center",color: "white", height:"100%" ,
+            alignItems:"center", display:"flex"}}>
+            <h2>Consultez ce site sur un ordinateur pour une meilleure expérience</h2>
+        </div>
+    )
+}
+
+export default SmallScreen;
