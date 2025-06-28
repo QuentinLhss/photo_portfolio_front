69de9d009f8dee6f812753d85ae2de3cebc10dfa
laho0011 <quentin.lahousse@etudiant.univ-reims.fr>
2025-06-28 16:32:43 +0200

    changement du boutton du formulaire de contact (ajout d'animations)

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,17 +1,22 @@
 import '../styles/ContactForm.scss';
+import * as motion from "motion/react-client"
 
 export default function ContactForm() {
     return (
         <div className="formContainer">
             <form className="form">
                 <div className="inputGroup">
-                    <input type="text" name="nom" placeholder="Nom" />
-                    <input type="text" name="prenom" placeholder="Prénom" />
+                    <input type="text" name="nom" placeholder="Nom"/>
+                    <input type="text" name="prenom" placeholder="Prénom"/>
                 </div>
-                <input type="email" name="email" placeholder="Adresse email" />
-                <textarea name="message" placeholder="Votre demande" />
-                <button type="submit">Envoyer</button>
+                <input type="email" name="email" placeholder="Adresse email"/>
+                <textarea name="message" placeholder="Votre demande"/>
+                <motion.div whileHover={{scale: 1.05}}
+                            whileTap={{scale: 0.95}}
+                            >
+                    <button type="submit">Envoyer</button>
+                </motion.div>
             </form>
         </div>
-    );
+);
 }
