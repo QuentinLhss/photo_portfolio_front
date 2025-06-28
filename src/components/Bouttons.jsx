9694bb3904fc61084f5ea253a4e96901d6d053b4
laho0011 <quentin.lahousse@etudiant.univ-reims.fr>
2025-06-28 16:32:21 +0200

    changement des bouttons (ajout d'animations)

--- src/components/Bouttons.jsx
+++ src/components/Bouttons.jsx
@@ -2,14 +2,21 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import "../styles/Bouttons.scss";
+import * as motion from "motion/react-client"
 
 export default function Bouttons() {
     const router = useRouter();
 
     return (
         <div className="buttons-container">
-            <button onClick={() => router.push("/plus")}>En voir plus</button>
-            <button onClick={() => router.push("/contact")}>Me contacter</button>
+            <motion.div whileHover={{scale: 1.1}}
+                        whileTap={{scale: 0.9}}>
+                <button onClick={() => router.push("/plus")}>En voir plus</button>
+            </motion.div>
+            <motion.div whileHover={{scale: 1.1}}
+                        whileTap={{scale: 0.9}}>
+                <button onClick={() => router.push("/contact")}>Me contacter</button>
+            </motion.div>
         </div>
-    );
+);
 }
